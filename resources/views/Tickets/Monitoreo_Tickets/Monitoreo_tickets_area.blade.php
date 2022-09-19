9c75078f30e45a1c12284af741f2b53fc779d94e
retrok218 <retrok218@gmail.com>
2022-09-19 17:58:53 -0500

    monitoreo areas

--- resources/views/Tickets/Monitoreo_Tickets/Monitoreo_tickets_area.blade.php
+++ resources/views/Tickets/Monitoreo_Tickets/Monitoreo_tickets_area.blade.php
@@ -1,43 +1,53 @@
 @extends('home')
+{{--<meta http-equiv="refresh" content="10"/>--}}
 @section('content')
 
 <div class="kt-container kt-container--fluid kt-grid__item kt-grid__item--fluid">
+    
     <div class="kt-portlet__head">
         <div class="header_titulo_monitoreo_tkts text-center" >
-            <div class="card-header" >
-                <span style="display: block; font-style: normal;  color: #2e2e2e; font-weight: 600; font-size: calc(0.8em + 1vw);">
-                    Tablero Mesa de Servicio  
-                                            
-                </span>
-                <span style="display: block; font-style: normal;  color: #2e2e2e; font-weight: 600; font-size: calc(0.8em + 1vw);">(Area / Asignados)</span>    
-            </div>          
+            
+                <div class="card-header"  style="background-image: url(../assets/media/logos/fondo3.jpg) !important;background-size: 100%;background-repeat: no-repeat;">
+                    <div class="containerglass ">
+                    <span style="display: block; font-style: normal;  color: #2e2e2e; font-weight: 600; font-size: calc(0.8em + 1vw);">
+                        Tablero Mesa de Servicio                                                  
+                    </span>
+                    <span style="display: block; font-style: normal;  color: #2e2e2e; font-weight: 600; font-size: calc(0.8em + 1vw);">(SIRADI)</span>    
+                </div>
+            </div>  
+
         </div>
     </div>
+
+
 <div class="kt-portlet">  
-    <div class="kt-container kt-container--fluid" style="background-color:white ;">
+ {{--Grafica de barras  --}}
+    <!-- <div class="kt-container kt-container--fluid" style="background-color:white ;">
     <div id="GraficaMonitoreoArea" style="height: 30em; width: 100%;"></div>
-    </div>
+    </div> -->
     
 <div class="kt-container kt-container--fluid" style="margin-top: 10px;">
     <div class="row">
         @foreach($datos_monitoreo_area as $datoarea)           
 
                     <div class="col-sm-6 col-md-6 col-lg-4">
-                        <div class="card bg-white p-3 mb-4 shadow">
-                            <div class="d-flex justify-content-between mb-4">
+                        <div class="card bg-white p-3 mb-4 shadow" style="padding: 0.5rem !important; margin-bottom: 0.5rem !important">
+                            {{--<div class="d-flex justify-content-between mb-4"> con margen de 4  --}} 
+                            <div class="d-flex justify-content-between ">
                                 <div class="user-info">
                                     
                                     <div class="user-info__basic">
-                                        <h5 class="mb-0">{{$datoarea->name}}</h5>									
+                                        <h6 class="mb-0" style="color: #bc955b;">{{$datoarea->name}}</h6>									
                                     </div>
                                 </div>
                               
                             </div>
+                            <hr>
                             <div class="kt-timeline-v3__item kt-timeline-v3__item--warning">
                                     
                                     <div class="kt-timeline-v3__item-desc">
                                         
-                                        <h6 class="mb-0" style="position: absolute;top: 50px;left: 45%;font-size: 2em;color: rgb(14 14 14 / 31%);pointer-events: none;"> 
+                                        <h6 class="mb-0" style="position: absolute;top: 70px;left: 65%;font-size: 2em;color: rgb(160 32 66 / 65%);pointer-events: none;"> 
                                             <div class="fas fa-ticket-alt fa-lg" id="ticketm"></div>
                                             {{$datoarea->tikets_area_grupo}}
                                         </h6>	 
@@ -46,7 +56,7 @@
                             </div>                                  
                                    
                             <div class="d-flex justify-content-between mt-4">
-                            <!-- <button class="btn btn-danger" type="submit" data-toggle="modal" data-target="#ConsultarGrupo" data-nombregrupo="{{$datoarea->name}}" data-idgrupo="{{$datoarea->id}}" onclick="" >Consultar</button> -->
+                           
                             <button onclick="subclases({{$datoarea->id}});" type="button" class="btn btn-outline-success">Consultar</button>
                                 
                             </div>
